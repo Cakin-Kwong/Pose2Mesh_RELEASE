09ea16efe621e68cbbade134036ae1cf009cc8a0
cakin <mrazjane@sina.com>
2022-01-19 20:09:30 +0800

    change simple baseline to sem_gcn

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -2,3 +2,6 @@ import models.pose2mesh_net
 import models.posenet
 import models.meshnet
 import models.project_net
+import models.graph_non_local
+import models.sem_graph_conv
+import models.sem_gcn
